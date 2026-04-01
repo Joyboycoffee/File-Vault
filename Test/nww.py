bfcc0fee29fb10745364235e97aae05c876f01e9
Gourav Kumar <106906124+Joyboycoffee@users.noreply.github.com>
2026-04-01 20:30:36 +0530

    Create nww.py

---
+++ Test/nww.py
@@ -0,0 +1,9 @@
+# Print a lot of stuff
+for i in range(10):
+    print(f"Line {i + 1}: This is some output")
+
+print("\n--- More stuff ---")
+for item in ["apples", "bananas", "cherries", "dates"]:
+    print(f"Item: {item}")
+
+print("\nDone!")
